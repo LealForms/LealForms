d69691cf181b837d0829e0792db1456e57116002
Swellshinider <eduardoleal.contact@gmail.com>
2024-10-09 20:22:22 -0300

    test gitignore

--- LForms/Program.cs
+++ LForms/Program.cs
@@ -1,15 +1,13 @@
 namespace LForms;
 
-static class Program
+internal static class Program
 {
     /// <summary>
     ///  The main entry point for the application.
     /// </summary>
     [STAThread]
-    static void Main()
+    internal static void Main()
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
         Application.Run(new Form1());
     }    
