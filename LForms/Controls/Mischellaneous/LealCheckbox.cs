9960f760ca1f580e0e1cf6c9fda6743a6c343935
Swellshinider <eduardoleal.contact@gmail.com>
2024-11-13 12:59:04 -0300

    Add LealCheckbox control and update related components
    Introduced the LealCheckbox class in the LForms.Controls.Mischellaneous namespace, inheriting from LealPanel. Added private fields and properties for customization, including text, label, checked state, rounded corners, gap, and label alignment. Implemented a constructor and custom drawing logic in the ReDraw method. Updated LealPanel comments for clarity. Added CheckboxLabelAlignment enum for label alignment options. Removed an unnecessary using directive from ControlExtensions.cs.

---
+++ LForms/Controls/Mischellaneous/LealCheckbox.cs
@@ -0,0 +1,94 @@
+﻿using LForms.Controls.Panels;
+using LForms.Enums;
+using LForms.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LForms.Controls.Mischellaneous;
+
+public class LealCheckbox : LealPanel
+{
+    private int _gap = 5;
+    private bool _rounded = false;
+    private bool _checked = false;
+    private CheckboxLabelAlignment _checkboxLabelAlignment = CheckboxLabelAlignment.CheckBoxLeftLabelRight;
+
+    private readonly Label _label;
+
+    public LealCheckbox(int width = 100, int height = 50) : base(false, true)
+    {
+        Size = new Size(width, height);
+        _label = new Label() 
+        {
+            AutoSize = false,
+            Text = "LealCheckbox",
+            Font = new Font("Rubik", 10, FontStyle.Regular),
+        };
+    }
+
+    public new string Text
+    {
+        get => _label.Text;
+        set
+        {
+            _label.Text = value;
+        }
+    }
+
+    public Label Label => _label;
+
+    public bool Checked
+    {
+        get => _checked;
+        set
+        {
+            _checked = value;
+            ReDraw();
+        }
+    }
+
+    public bool Rounded
+    {
+        get => _rounded;
+        set
+        {
+            _rounded = value;
+            ReDraw();
+        }
+    }
+
+    public int Gap
+    {
+        get => _gap;
+        set
+        {
+            _gap = value;
+            ReDraw();
+        }
+    }
+
+    public CheckboxLabelAlignment CheckboxLabelAlignment
+    {
+        get => _checkboxLabelAlignment;
+        set
+        {
+            _checkboxLabelAlignment = value;
+            ReDraw();
+        }
+    }
+
+    protected override void ReDraw()
+    {
+
+
+        if (_rounded)
+            this.GenerateRoundRegion();
+        else
+            Region = null;
+    }
+}
