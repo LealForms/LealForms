644aeed86dea4291a677c165e7472b0155afd381
Swellshinider <eduardoleal.contact@gmail.com>
2024-11-14 12:10:48 -0300

    Added `CheckboxCharacterType` enum in `CheckboxCharacterType.cs` to define various characters for the checkbox mark, such as check mark, cross mark, dot mark, star mark, heart mark, square mark, circle mark, and triangle mark.
    Added `CheckboxType` enum in `CheckboxType.cs` to define the shape of the checkbox, allowing either a square or circular style.

---
+++ LForms/Enums/CheckboxCharacterType.cs
@@ -0,0 +1,47 @@
+﻿namespace LForms.Enums;
+
+/// <summary>
+/// Specifies the different types of characters that can be used for checkboxes.
+/// </summary>
+public enum CheckboxCharacterType
+{
+    /// <summary>
+    /// A check mark character.
+    /// </summary>
+    CheckMark,
+
+    /// <summary>
+    /// A cross mark character.
+    /// </summary>
+    CrossMark,
+
+    /// <summary>
+    /// A dot mark character.
+    /// </summary>
+    DotMark,
+
+    /// <summary>
+    /// A star mark character.
+    /// </summary>
+    StarMark,
+
+    /// <summary>
+    /// A heart mark character.
+    /// </summary>
+    HeartMark,
+
+    /// <summary>
+    /// A square mark character.
+    /// </summary>
+    SquareMark,
+
+    /// <summary>
+    /// A circle mark character.
+    /// </summary>
+    CircleMark,
+
+    /// <summary>
+    /// A triangle mark character.
+    /// </summary>
+    TriangleMark,
+}
---
+++ LForms/Enums/CheckboxType.cs
@@ -0,0 +1,17 @@
+﻿namespace LForms.Enums;
+
+/// <summary>
+/// Defines the types of checkboxes available.
+/// </summary>
+public enum CheckboxType
+{
+    /// <summary>
+    /// Represents a checkbox with a square shape.
+    /// </summary>
+    Square,
+
+    /// <summary>
+    /// Represents a checkbox with a circular shape.
+    /// </summary>
+    Circle
+}
